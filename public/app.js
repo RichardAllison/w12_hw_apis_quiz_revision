b915a1d266c349985fca8d905f6de3cf56eb168b
Richard Allison <richard.allison.18@outlook.com>
2018-02-23 23:47:47 +0000

    Refactored code by breaking it up into smaller components

--- public/app.js
+++ public/app.js
@@ -1,36 +1,126 @@
 // ----------------
-const app = function() {
+document.addEventListener("DOMContentLoaded", function() {
+  if (localStorage.getItem('state')) {
+    if (localStorage.getItem('state') === "Geography") {
+      showGeographySection();
+    }
+  } else {
 
-  const geographyButton = document.getElementById("geography-button");
-  geographyButton.addEventListener("click", showGeographySection);
+    const geographyButton = document.getElementById("geography-button");
+    geographyButton.addEventListener("click", showGeographySection);
+  }
+  createParagraph(document.querySelector("footer"), `${'\u00A9'} Richard Allison ${(new Date().getFullYear())}. All rights reserved.`)
+});
+// ----------------
+const createParagraph = function(appendTo, text) {
+  const p = document.createElement("p");
+  p.innerText = text;
+  appendTo.appendChild(p);
+  return p;
+}
+
+const createLabel = function(appendTo, id, labelText) {
+  const label = document.createElement("label");
+  label.setAttribute("for", id);;
+  label.innerText = labelText;
+  appendTo.appendChild(label);
+  return label;
+}
+
+const createSelect = function(appendTo, id, labelText) {
+  if (labelText) {
+    createLabel(appendTo, id, labelText)
+  }
+  const select = document.createElement("select");
+  select.id = id;
+  select.name = id;
+  appendTo.appendChild(select);
+  return select;
+}
+
+const createOption = function(appendTo, text, selected, disabled, value) {
+  const option = document.createElement("option");
+  option.innerText = text;
+  if (value) {
+    option.value = value;
+  }
+  if (disabled) {
+    option.disabled = disabled;
+  }
+  if (selected) {
+    option.setAttribute("selected", true);
+  }
+  appendTo.appendChild(option);
+  return option;
+}
+
+const createImage = function(url, appendTo) {
+  const img = document.createElement("img");
+  img.src = url;
+  img.width = "200";
+  appendTo.appendChild(img);
+  return img;
+}
+
+const createList = function(appendTo) {
+  const ul = document.createElement("ul");
+  appendTo.appendChild(ul);
+  return ul;
+}
+
+const createListItem = function(appendTo, innerText) {
+  const li = document.createElement("li");
+  if (innerText) {
+    li.innerText = innerText;
+  }
+  appendTo.appendChild(li);
+  return li;
+}
+
+const rememberState = function(state) {
+  localStorage.setItem('state', state);
 }
 // ----------------
 const showGeographySection = function(){
-  document.getElementById("display-section").innerHTML =
-    `<p>View information about a country</p>
-    Region: <select id="region-select">
-      <option selected disabled>Select a region</option>
-    </select>
-    Country: <select id="country-select">
-      <option selected disabled>Select a country to view information</option>
-    </select>
-    <section id="country-information-section">
-      <div id="country-info">
+  rememberState("Geography");
 
-      </div>
-      <div id="main-map">
+  const heading = document.querySelector("h1");
+  heading.innerText = "Revise Geography";
 
-      </div>`
+  const subTitle = document.querySelector("header p");
+  subTitle.innerText = "View information about a country";
 
-  const url = "https://restcountries.eu/rest/v2/all"
-  makeRequest(url, requestComplete);
+  // const reviseSection = document.getElementById("revise-section")
+  // reviseSection.parentNode.removeChild(reviseSection);
 
-  const mapDiv = document.querySelector("#main-map");
+  const display = document.getElementById("display-section");
+
+  const regionSelect = createSelect(display, "region-select", "Region: ")
+  createOption(regionSelect, "Select a region", true, true)
+
+  const select = createSelect(display, "country-select", "Country: ");
+  createOption(select, "Select a country", true, true)
+
+  const countryInfoSection = document.createElement("section");
+  countryInfoSection.id = "country-information-section";
+  display.appendChild(countryInfoSection);
+
+  const countryInfoDiv = document.createElement("div");
+  countryInfoDiv.id = "country-info"
+  countryInfoSection.appendChild(countryInfoDiv);
+
+  const mapDiv = document.createElement("div")
+  mapDiv.id="main-map";
+  document.getElementById("display-section").appendChild(mapDiv);
+
   const coords = {lat: 0, lng: 0}
-  const map = new MapWrapper(mapDiv, coords, 1);
-}
+  const map = new MapWrapper(mapDiv, coords, 2);
 
-const makeRequest = function(url, callback) {
+  makeRequest(requestComplete);
+}
+// ----------------
+const makeRequest = function(callback) {
+  const url = "https://restcountries.eu/rest/v2/all"
   const request = new XMLHttpRequest();
   request.open("GET", url);
   request.addEventListener("load", callback);
@@ -60,15 +150,22 @@ const populateList = function(countries){
   });
 };
 
+// ----------------
 const displayCountryInfo = function(countries) {
   const index = document.getElementById('country-select').value;
   const country = countries[index];
+
+  const display = document.getElementById("country-info")
+  display.innerText = "";
+  const infoList = createList(display);
+
+  createListItem(infoList, country.name);
+  const flagLi = createListItem(infoList);
+  createImage(country.flag, flagLi);
+  createListItem(infoList, country.capital);
 
   const coords = {lat: country.latlng[0], lng: country.latlng[1]};
   const mapDiv = document.querySelector('#main-map');
   const map = new MapWrapper(mapDiv, coords, 4);
   map.addMarker(coords);
 }
-
-// ----------------
-document.addEventListener("DOMContentLoaded", app);
